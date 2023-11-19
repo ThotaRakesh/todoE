67093ad4df2dd718482a33a654bcb08718be269e
rakesh <thotarakesh28@gmail.com>
2023-11-19 05:12:01 +0000

    solution

---
+++ src/main/java/com/example/todo/model/Todo.java
@@ -0,0 +1,54 @@
+package com.example.todo.model;
+
+public class Todo {
+    private int id;
+    private String todo;
+    private String priority;
+    private String status;
+    
+
+    // Constructors
+
+    public Todo(int id, String todo, String status, String priority) {
+        this.id = id;
+        this.todo = todo;
+        this.priority = priority;
+        this.status = status;
+       
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public String getPriority() {
+            return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    
+}
